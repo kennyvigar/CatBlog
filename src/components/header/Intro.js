795b2f004c5ccc52f1a7d5ee3d79dd21fd849a0a
kenny vigar <kvigar@gmail.com>
2022-08-21 16:08:12 -0600

    cleaning and overhauling style

--- src/components/header/Intro.js
+++ src/components/header/Intro.js
@@ -1,12 +1,14 @@
 import React from "react";
-import styles from "../cardHolder/cardHolder.module.css";
+//import styles from "../cardHolder/cardHolder.module.css";
 
+import styles from "../header/header.module.css";
 export default (props) => (
   <div className={styles.center}>
     <div className={styles.intro}>
-      <p>My name is eveline, and I have a a cat blog</p>
-      <p>I think cats are cool, but I love my dog Franklin a lot!!!!!</p>
+      
+      <p>My name is eveline, and I have a a cat blog. I think cats are cool, but I love my dog Franklin a lot!!!!</p> 
       <p>I also really like</p>
+
       <ul>
         <li>cats</li>
         <li>bacon</li>
