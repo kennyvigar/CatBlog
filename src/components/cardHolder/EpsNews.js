bbab615dbfa9bed66187aaaded74eb43566a7ab6
Kenny Vigar <kennyvigar@Kennys-MacBook-Air.local>
2024-01-25 07:17:00 -0700

    (blog):another joke and pic - I know.. I know.. another direct to main push..

--- src/components/cardHolder/EpsNews.js
+++ src/components/cardHolder/EpsNews.js
@@ -11,6 +11,19 @@ const epsNews = () => {
         <div className={styles.epsnews}>
             
             <div className={styles.blogBrick1}>
+                <h2>January 25 </h2>
+                <p></p>
+                <p>I have a good joke</p>
+                <br></br>
+                <p>joke of the day:</p>
+                <br></br>
+                <p>why did bike fall over?</p>
+                <p>because it was too tired</p>
+                <img src={process.env.PUBLIC_URL + 'pictures/garneau-gros-louis-2-fat-bike.webp'} alt="My Image" />
+
+            </div>
+
+            <div className={styles.blogBrick1}>
                 <h2>January 24 </h2>
                 <p>happy new year</p>
                 <p>something new, i am going to add is</p>
